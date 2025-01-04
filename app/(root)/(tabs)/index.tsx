69d718afc2fd087ca2fba8fec0eb482bb0bd0cae
nagxsan <sanchetng@gmail.com>
2025-01-04 15:29:46 +0530

    nit fixes

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {View, Text, Image, TouchableOpacity, ScrollView} from 'react-native'
+import {View, Text, Image, TouchableOpacity, ScrollView, FlatList} from 'react-native'
 import React from 'react'
 import {SafeAreaView} from "react-native-safe-area-context";
 import {useGlobalContext} from "@/lib/global-provider";
@@ -12,48 +12,59 @@ const Home = () => {
   const { user } = useGlobalContext();
   return (
     <SafeAreaView className="bg-white h-full">
-      <ScrollView showsVerticalScrollIndicator={false} contentContainerClassName={"pb-32"}>
-        <View className="px-5 flex flex-col gap-y-5">
-          <View className={"flex flex-row justify-between items-center mt-5"}>
-            <View className={"flex flex-row items-center justify-center gap-x-3"}>
-              <Image source={{ uri: user?.avatar}} className={"size-12 relative rounded-full"} />
-              <View className={"flex flex-col items-start justify-center"}>
-                <Text className={"text-xs font-rubik text-black-100"}>Good Morning</Text>
-                <Text className={"text-base font-rubik-medium text-black-300"}>{user?.name}</Text>
+      <FlatList
+        data={[1, 2,3,4]}
+        renderItem={({item}) => <Card />}
+        keyExtractor={(item, index) => item.toString()}
+        numColumns={2}
+        contentContainerClassName={"pb-32"}
+        columnWrapperClassName={"flex gap-5 px-5"}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View className="px-5 flex flex-col gap-y-5">
+            <View className={"flex flex-row justify-between items-center mt-5"}>
+              <View className={"flex flex-row items-center justify-center gap-x-3"}>
+                <Image source={{ uri: user?.avatar}} className={"size-12 relative rounded-full"} />
+                <View className={"flex flex-col items-start justify-center"}>
+                  <Text className={"text-xs font-rubik text-black-100"}>Good Morning</Text>
+                  <Text className={"text-base font-rubik-medium text-black-300"}>{user?.name}</Text>
+                </View>
               </View>
+              <Image source={icons.bell} className={"size-6"} />
             </View>
-            <Image source={icons.bell} className={"size-6"} />
-          </View>
-          <Search />
-          <View>
+            <Search />
+            <View>
+              <View className={"flex flex-row items-center justify-between"}>
+                <Text className={"text-xl font-rubik-bold text-black-300"}>Featured</Text>
+                <TouchableOpacity>
+                  <Text className={"text-base font-rubik-bold text-primary-300"}>
+                    See All
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <FlatList
+                data={[1,2,3]}
+                renderItem={({item}) => <FeaturedCard />}
+                keyExtractor={(item, index) => item.toString()}
+                horizontal={true}
+                bounces={false}
+                showsHorizontalScrollIndicator={false}
+                contentContainerClassName={"flex gap-5 mt-5"}
+              />
+            </View>
             <View className={"flex flex-row items-center justify-between"}>
-              <Text className={"text-xl font-rubik-bold text-black-300"}>Featured</Text>
+              <Text className={"text-xl font-rubik-bold text-black-300"}>Our recommendations</Text>
               <TouchableOpacity>
                 <Text className={"text-base font-rubik-bold text-primary-300"}>
                   See All
                 </Text>
               </TouchableOpacity>
             </View>
+            <Filters />
           </View>
-          <View className={"flex flex-row gap-5 my-3"}>
-            <FeaturedCard />
-            <FeaturedCard />
-          </View>
-          <View className={"flex flex-row items-center justify-between"}>
-            <Text className={"text-xl font-rubik-bold text-black-300"}>Our recommendations</Text>
-            <TouchableOpacity>
-              <Text className={"text-base font-rubik-bold text-primary-300"}>
-                See All
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <Filters />
-          <View className={"flex flex-row gap-5 my-2"}>
-            <Card />
-            <Card />
-          </View>
-        </View>
-      </ScrollView>
+        }
+      />
+      
     </SafeAreaView>
   )
 }
--- components/Cards.tsx
+++ components/Cards.tsx
@@ -14,7 +14,7 @@ export const FeaturedCard = ({ onPress } : CardsProps) => {
       <Image source={images.cardGradient} className={"size-full rounded-2xl absolute bottom-0"} />
       <View className="flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5">
         <Image source={icons.star} className={"size-3.5"} />
-        <Text className={"text-xs font-rubik-bold text-primary-300 ml-1"}>4.4</Text>
+        <Text className={"text-xs font-rubik-bold text-primary-300 ml-1 mt-1"}>4.4</Text>
       </View>
       <View className={"flex flex-col items-start absolute bottom-5 inset-x-5"}>
         <Text className={"text-xl font-rubik-extrabold text-white"} numberOfLines={1}>Modern Apartment</Text>
@@ -30,10 +30,10 @@ export const FeaturedCard = ({ onPress } : CardsProps) => {
 
 export const Card = ({ onPress } : CardsProps) => {
   return (
-    <TouchableOpacity onPress={onPress} className={"flex-1 w-full px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative"}>
+    <TouchableOpacity onPress={onPress} className={"flex-1 w-full px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-300 relative mt-5"}>
       <View className="flex flex-row items-center absolute top-5 right-5 bg-white/90 px-2 py-1 rounded-full z-50">
         <Image source={icons.star} className={"size-2.5"} />
-        <Text className={"text-xs font-rubik-bold text-primary-300 ml-0.5"}>4.4</Text>
+        <Text className={"text-xs font-rubik-bold text-primary-300 ml-0.5 mt-1"}>4.4</Text>
       </View>
       <Image source={images.newYork} className={"w-full h-40 rounded-lg"} />
       <View className={"flex flex-col mt-2"}>
